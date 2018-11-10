2b9079fa00f8651ff63dc9098619d56632b5a6f4
sengathit <sengathit.l@gmail.com>
2018-11-09 22:19:22 -0600

    added lib folders, added setup dependencies

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get('/',(req,res) => {
     res.render('home',{title : 'home'});
 });
 
+router.get('/about-me',(req,res) => {
+    res.render('about-me',{title : 'about me'});
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
+const bodyParser = require('body-parser');
+// const bootstrap = require('bootstrap');
 
 const app = express();
 
 const port = 3000;
 
+// Routes for the application
 const index = require('./routes/index');
 
-app.set('views',path.join(__dirname,'views'))
+// Set the template engine
+app.set('views',path.join(__dirname,'views'));
 app.set('view engine','hbs');
 
 hbs.registerPartials(__dirname + '/views/partials');
 
+// Serve static files
+app.use(express.static(__dirname + '/public'));
+
+// Parse all incoming request
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+// Load the routes
 app.use('/',index);
 
 app.listen(port,() => console.log('Server is started on port ' + port));
